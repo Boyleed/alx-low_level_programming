fb59f2daf0d11ef0a9d62d769e302f9ad1ad08b1
Boyleed <boyleigierobo@gmail.com>
2022-09-09 22:26:52 -0700

    Boyleed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,9 +7,9 @@
  **Return: Always 0.
  **/
 int main(void)
-
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
@@ -18,6 +18,6 @@ int main(void)
 	else if ((n % 10) < 6 && (n % 10) != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	else
-		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	return (0);
 }
