ebbd53f883b6acd828857266d9601e0e8199fdca
Boyleed <boyleigierobo@gmail.com>
2022-09-09 19:09:24 -0700

    Boyleed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -24,17 +24,16 @@ int main(void)
 
 
 
-					if (n > 0)
+	if (n > 0)
+		
+		printf("%d is positive\n", n);
 
-								printf("%d is positive\n", n);
+	else if (n < 0)
 
-						else if (n < 0)
+		printf("%d is negative\n", n);
 
-									printf("%d is negative\n", n);
-
-							else
-
-										printf("%d is zero\n", n);
+	else
+		printf("%d is zero\n", n);
 
 
 
