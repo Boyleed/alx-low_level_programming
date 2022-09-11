a513cddd6d382fc793800968e21dba107eba0f4d
Boyleed <boyleigierobo@gmail.com>
2022-09-10 21:13:41 -0700

    Boyleed

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,7 +5,6 @@
 int main(void)
 {
 	int num;
-	
 	char letter;
 
 	for (num = 0; num < 10; num++)
@@ -13,5 +12,6 @@ int main(void)
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
 	putchar('\n');
+
 	return (0);
 }
