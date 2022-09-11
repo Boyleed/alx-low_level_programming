c9f16d4712d495e9dbd4036f8e3da4dc1ba9d7f6
Boyleed <boyleigierobo@gmail.com>
2022-09-10 21:19:54 -0700

    Boyleed

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
- **main - Prints all the numbers of base 16 in lowercase. **Return: Always 0.
+ **main - Prints all the numbers of base 16 in lowercase.
+ **Description - 'the program's description'
+ **Return: Always 0.
  **/
 int main(void)
 {
