2faf2ecea3540e44cca95aa07f8ad0bb22f1b911
Boyleed <boyleigierobo@gmail.com>
2022-09-11 21:59:58 -0700

    Boyleed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,12 +7,12 @@ int main(void)
 {
 	int d;
 
-	for (d = 0; d <= 100; d++)
+	for (d = 0; d <= 89; d++)
 {
 	putchar((d / 10) + '0');
 	putchar((d % 10) + '0');
 
-		if (d != 99)
+		if (d != 89)
 		{
 			putchar(',');
 			putchar(' ');
