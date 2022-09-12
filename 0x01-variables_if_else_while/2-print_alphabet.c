efa563e31afd06ba0e9bc986249c29bb64d68455
Boyleed <boyleigierobo@gmail.com>
2022-09-11 20:28:39 -0700

    Boyleed

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,15 +7,9 @@ int main(void)
 {
 	int num;
 	
-	for (num = 0; num <= 9; num++)
-{
-		putchar((num % 10) + '0');
-	if (num == 9, continue;)
+	for (num = 'a' ; num <= 'z' ; num++)
+		putchar(num);
 
-	putchar(',');
-	putchar(' ');
-}
 	putchar('\n');
-	
 	return (0);
 }
