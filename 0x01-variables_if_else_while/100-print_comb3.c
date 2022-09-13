b6c0fbadf047729abb2865acebcf549d79f5d63d
Boyleed <boyleigierobo@gmail.com>
2022-09-12 20:26:44 -0700

    Boyled

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,19 +5,27 @@
  **/
 int main(void)
 {
-	int d;
-
-	for (d = 0; d <= 89; d++)
-{
-	putchar((d / 10) + '0');
-	putchar((d % 10) + '0');
-
-		if (d != 89)
+	int d1 = 0, d2;
+	while(d1 <=9)
+	{
+		d2 = 0;
+		while(d2 <= 9)
 		{
-			putchar(',');
-			putchar(' ');
+			if (d1 != d2 && d1 < d2)
+		{
+			putchar(d1 + 48);
+			putchar(d2 + 48);
+			
+			if (d1 + d2 != 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
-}
+		++d2;
+		}
+		++d1;
+	}
 	putchar('\n');
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ ** main - Prints all the numbers of base 16 in lowercase
+ ** Return: Always 0.
+ **/
+int main(void)
+{
+	int b1 = 0, b2;
+
+	while(b1 <= 99)
+	{
+		b2 = b1;
+		while(b2 <= 99)
+		{
+			if(b2 != b1)
+			{
+				putchar((b1 / 10) + 48);
+				putchar((b1 % 10) + 48);
+				putchar(' ');
+				putchar((b2 / 10) + 48);
+				putchar((b2 % 10) + 48);
+			if(b1 != 98 || b2 !=98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		++b2
+		}
+		++b1
+	}
+	putchar('\n');
+	return (0);
+}
