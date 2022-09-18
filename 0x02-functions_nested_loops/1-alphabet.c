c3591d7dec3fa5118207ee69d871f14fdaec6b10
Boyleed <boyleigierobo@gmail.com>
2022-09-18 07:13:15 -0700

    Bool

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  ** print_alphabet – function to print abc
- ** Return: 0
+ ** Description - 'function'
+ **Return: 0
  **/
 void print_alphabet(void)
 {
 	int n;
-
+	
 	for (n = 'a'; n <= 'z'; n++)
 	{
 		 _putchar(n);
