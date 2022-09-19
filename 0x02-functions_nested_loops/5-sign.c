bf708069668886d3d112b6289b9757577e1804fb
Boyleed <boyleigierobo@gmail.com>
2022-09-18 19:56:25 -0700

    Bool

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
- * * print_sign – function to check for a sign of a number
- * * @n:  is the int that will use for the argument of the function
- * * return: 0
- * */
+ * print_sign – function to check for a sign of a number
+ *
+ * @n:  is the int that will use for the argument of the function
+ *
+ * return: 0
+ */
 int print_sign(int n)
 {
 	if (n > 0)
