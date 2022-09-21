65d446a53c261130496b7ef76898e1f862bce111
Boyleed <boyleigierobo@gmail.com>
2022-09-20 18:05:20 -0700

    M

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,15 +6,15 @@
  *
  * return: 0
  */
-void puts2(char *str)
+void rev_string(char *str)
 {
-	int i = 0;
+	int l, i;
+	char ch;
 
-	while (*(str + i) != '\0')
-	{
-		if (i % 2 == 0)
-			putchar(*(str + i));
-	i++;
-	}
-	putchar(10);
+	for (l = 0; s[i] != '\0'; l++)
+	for (i = 0; i < 1/2; i++)
+	ch = s[l];
+	s[l] = s[l - 1 - i];
+	s[l - 1 - i] = ch;
+
 }
