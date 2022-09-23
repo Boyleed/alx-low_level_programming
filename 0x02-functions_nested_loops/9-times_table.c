246b87af7001285b7d0f90ea1a94030e41c0bd41
Boyleed <boyleigierobo@gmail.com>
2022-09-22 22:00:44 -0700

    M

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,26 +1,38 @@
+#include <unistd.h>
 #include "main.h"
 /**
  *times_table - Prints the 9 times table, starting with 0.
  */
 void times_table(void)
 {
-	int num, mult, prod;
+	int x, y, a, b;
 
-	for (num = 0; num <= 9; num++)
+	for (x = 0; x < 10; x++)
 	{
-		 _putchar('0');
-		 for (mult = 1; mult <= 9; mult++)
-		 { 
-			 _putchar(',');
-			 _putchar(' ');
-			 prod = num * mult;
-			 if (prod <= 9)
-				 _putchar(' ');
-			 else
-				 _putchar((prod / 10) + '0');
-			 _putchar((prod % 10) + '0');
-		 }
-		   _putchar('\n');
+		for (y = 0; y < 10; y++)
+		{
+			a = (x * y) / 10;
+			b = (x * y) % 10;
+
+			if ((x * y) > 9)
+				_putchar(a + '0');
+
+			_putchar(b + '0');
+
+			/* Don't print commas after the 10th column */
+			if (y != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			/* Add an extra space if printing single digits */
+			if (y == 9 && x == 0)
+				break;
+			else if (x * (y + 1) < 10)
+				_putchar(' ');
+		}
+		_putchar('\n');
 	}
 }
 
