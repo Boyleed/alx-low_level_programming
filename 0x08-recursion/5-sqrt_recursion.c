7f07f4911567102ab6f74f8f3f93372b448ee753
Boyleed <boyleigierobo@gmail.com>
2022-09-28 20:19:09 -0700

    M

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,7 +17,6 @@ int find_root(int n, int root)
 
 	if (root * root == n)
 		return (root);
-	
 	return (find_root(n, root + 1));
 }
 
@@ -34,7 +33,5 @@ int _sqrt_recursion(int n)
 
 	if (n < 0)
 		return (-1);
-	
 	return (find_root(n, 0));
-
 }
